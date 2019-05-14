b5bf1081e476f4314d4a73b9538fb23cebbdeb9d
nalquas <22664233+nalquas@users.noreply.github.com>
2019-05-14 17:45:49 +0200

    Implemented on-screen buttons

--- Gasteroids/scripts/Game_GUI.gd
+++ Gasteroids/scripts/Game_GUI.gd
@@ -1,7 +1,17 @@
 extends Control
 
+var pressedThrust = false
+var pressedLeft = false
+var pressedRight = false
+var pressedShoot = false
+
 func _ready():
-	pass
+	#If on mobile, show touch overlay (buttons):
+	var name = OS.get_name()
+	if name=="Android" or name=="iOS" or name=="UWP":
+		setTouchOverlay(true)
+	else:
+		setTouchOverlay(true)
 
 func setGameOver(gameover):
 	$GameOver.visible = gameover
@@ -12,3 +22,37 @@ func setScore(score):
 func setRespawns(respawns):
 	$Respawns.margin_right=131*respawns
 	$Respawns.rect_size=Vector2(131*respawns,131)
+
+func setTouchOverlay(show):
+	$Thrust.visible = show
+	$Thrust.disabled = !show
+	$Shoot.visible = show
+	$Shoot.disabled = !show
+	$Left.visible = show
+	$Left.disabled = !show
+	$Right.visible = show
+	$Right.disabled = !show
+
+func _on_Thrust_button_down():
+	pressedThrust = true
+
+func _on_Thrust_button_up():
+	pressedThrust = false
+
+func _on_Shoot_button_down():
+	pressedShoot = true
+
+func _on_Shoot_button_up():
+	pressedShoot = false
+
+func _on_Left_button_down():
+	pressedLeft = true
+
+func _on_Left_button_up():
+	pressedLeft = false
+
+func _on_Right_button_down():
+	pressedRight = true
+
+func _on_Right_button_up():
+	pressedRight = false
--- Gasteroids/scripts/Ship.gd
+++ Gasteroids/scripts/Ship.gd
@@ -29,18 +29,30 @@ func _process(delta):
 			$AnimatedSprite.scale=Vector2(0,0)
 	
 	if running:
+		#Get Touch input map
+		
+		var touchThrust = false
+		var touchLeft = false
+		var touchRight = false
+		var touchShoot = false
+		if "Game" in get_parent().name:
+			touchThrust = get_parent().get_node("Game_GUI").pressedThrust
+			touchLeft = get_parent().get_node("Game_GUI").pressedLeft
+			touchRight = get_parent().get_node("Game_GUI").pressedRight
+			touchShoot = get_parent().get_node("Game_GUI").pressedShoot
+		
 		#Immunity deactivation
 		if waitingForImmuneStop and OS.get_system_time_msecs()-t_immune>3000:
 			setImmune(false)
 		
 		#Rotation
-		if Input.is_action_pressed("ship_left"):
+		if Input.is_action_pressed("ship_left") or touchLeft:
 			rotation_degrees-=delta*rotation_factor
-		elif Input.is_action_pressed("ship_right"):
+		elif Input.is_action_pressed("ship_right") or touchRight:
 			rotation_degrees+=delta*rotation_factor
 		
 		#Thrusting
-		if Input.is_action_pressed("ship_thrust"):
+		if Input.is_action_pressed("ship_thrust") or touchThrust:
 			speed+=Vector2(sin(rotation),cos(rotation))*thrust_factor
 			if waitingForImmuneStop:
 				playAnimation("boost_immune")
@@ -53,7 +65,7 @@ func _process(delta):
 				playAnimation("default")
 		
 		#Firing
-		if Input.is_action_pressed("ship_shoot") and OS.get_system_time_msecs()-t_shot>333:
+		if (Input.is_action_pressed("ship_shoot") or touchShoot) and OS.get_system_time_msecs()-t_shot>333:
 			emit_signal("shoot")
 			t_shot=OS.get_system_time_msecs()
 
